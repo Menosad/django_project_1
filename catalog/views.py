4d67ccb5e52545c784e4c7a4aacf545ffcb2eb18
menosad <draignair@gmail.com>
2024-06-02 16:25:45 +0300

    fix settings, add template contacts.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
+import os
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, os.path.join('catalog', 'index.html'))
+
+
+def contacts(request):
+    return render(request, os.path.join('catalog', 'contacts.html'))
