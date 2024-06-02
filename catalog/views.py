7e29739d36c628bfd0d9dd55d2398dea6fcfbf27
menosad <draignair@gmail.com>
2024-06-02 20:14:58 +0300

    The first attempt, the application is ready

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,12 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('UserName')
+        email = request.POST.get('UserEmail')
+        phone_number = request.POST.get('UserPhone')
+        print(f"FEEDBACK request:\n"
+              f"user: {name}\n"
+              f"email: {email}\n"
+              f"phone number: {phone_number}")
     return render(request, os.path.join('catalog', 'contacts.html'))
