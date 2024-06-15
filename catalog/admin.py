4054a4c43ca30bd5dfdba27775e3b90d8b781624
menosad <draignair@gmail.com>
2024-06-15 11:49:11 +0300

    added a display to the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
 @admin.register(Category)
 class CatalogAdmin(admin.ModelAdmin):
     list_display = ('name', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
 
--- catalog/models.py
+++ catalog/models.py
@@ -19,11 +19,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='название')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Category')
     avatar = models.ImageField(**NULLABLE, verbose_name='картинка', upload_to='products')
     price = models.IntegerField(verbose_name='цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    #manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта', **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
