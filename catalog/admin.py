231fb88b6edc5e5fedca4c64baf7725fb3f46ce0
menosad <draignair@gmail.com>
2024-06-15 10:14:15 +0300

    added models Category, Product: migration initial

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from catalog.models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+
