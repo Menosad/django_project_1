e76ee9e8271b60d44f12ab2f7310488af1fd53d9
menosad <draignair@gmail.com>
2024-06-28 15:42:13 +0300

    1 step finished

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import MyappConfig
-from catalog.views import product, CatalogListView, CatalogTemplateView
+from catalog.views import CatalogListView, CatalogTemplateView, ProductDetailView
 
 app_name = MyappConfig.name
 
 urlpatterns = [
     path('', CatalogListView.as_view(), name='index'),
     path('contacts/', CatalogTemplateView.as_view(), name='contacts'),
-    path('product/<int:pk>/', product, name='product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
 from catalog.models import Product
 
@@ -9,31 +9,25 @@ class CatalogListView(ListView):
     template_name = 'catalog/index.html'
 
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('UserName')
-#         email = request.POST.get('UserEmail')
-#         phone_number = request.POST.get('UserPhone')
-#         print(f"FEEDBACK request:\n"
-#               f"user: {name}\n"
-#               f"email: {email}\n"
-#               f"phone number: {phone_number}")
-#     context = {
-#         'title': 'Контакты'
-#     }
-#     return render(request, 'catalog/contacts.html', context)
 class CatalogTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
 
-    def post(self, form):
-        if form == 'POST':
-            print('POST')
+    def post(self, *args, **kwargs):
+        name = self.request.POST.get('UserName')
+        mail = self.request.POST.get('UserEmail')
+        phone = self.request.POST.get('UserPhone')
+        print(f"Запрос на обратную связь:\n{name}\n{mail}\n{phone}")
+        return render(self.request, self.template_name)
 
 
-def product(request, pk):
-    prod = get_object_or_404(Product, pk=pk)
-    context = {
-        'prod': prod,
-        'title': prod.name
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+
+# def product(request, pk):
+#     prod = get_object_or_404(Product, pk=pk)
+#     context = {
+#         'prod': prod,
+#         'title': prod.name
+#     }
+#     return render(request, 'catalog/product.html', context)
